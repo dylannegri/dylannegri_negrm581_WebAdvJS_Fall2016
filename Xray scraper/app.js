74c584c8b1c2becf1877e3be76118a77e0c922c4
dylannegri <negrm581@newschool.edu>
2016-10-04 16:40:13 -0400

    xray

--- Xray scraper/app.js
+++ Xray scraper/app.js
@@ -4,12 +4,15 @@ var x = Xray();
 // 1st argument: URL you want to scrape
 // 2nd argument: the selector that you want to grab
 // 3rd argument: the array containing object of what you want to be passed on
-x('https://www.rottentomatoes.com/browse/in-theaters/', '.mb-movies', [{
-	topMovies: x('.mb-movie', [{
-		title: '.movie_info a .movieTitle',
-		//link: '.movie_info a@href',
+x('http://www.billboard.com/charts/hot-100', '.container', [{
+	topSongs: x('.chart-row__primary', [{
+		title: '.chart-row__container .chart-row__title .chart-row__song',
+		artist: '.chart-row__container .chart-row__title a@href',
 		// year: '.lister-item-year',
-		// rank: '.lister-item-index',
+		currentRank: '.chart-row__rank .chart-row__current-week',
+		lastWeekRank: '.chart-row__last-week',
+		poster: '.chart-row__image@style',
+		poster2: '.chart-row__image@data-imagesrc',
 		// rating: '.ratings-imdb-rating strong',
 		// thumbnail: '.lister-item-image img@src',
 		// genre: ['.genre'],
