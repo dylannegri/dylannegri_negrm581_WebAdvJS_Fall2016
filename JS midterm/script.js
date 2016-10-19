c467c73a96980e3ef442f9b38d68e76752d0a4e9
dylannegri <negrm581@newschool.edu>
2016-10-19 12:42:16 -0400

    midterm

--- JS midterm/script.js
+++ JS midterm/script.js
@@ -1,13 +1,58 @@
-//jquery bitches!
-$(document).ready(function(){
-	console.log('Ready!');
+// var app = app || {};
+
+// app.main = (function() {
+///////////////////////////
+	
 	$(function(){
 		$.getJSON('results.json', function(data){
-			console.log("getting json");
+			console.log(data);
 			var template = $('#template').html();
-			var html = Mustache.render(template, data);
-			$('#results').html(html);
-		});
-	});
-}); //ready!
+			var input = Mustache.render(template, data);
+			$('#results').html(input);
+			console.log("attempting to apply results");
+
+			$('#results').cycle({
+				fx: 'fade',
+				pause: 1,
+				next: '#next_btn',
+				prev: '#prev_btn',
+				speed: 500,
+				timeout: 10000
+			}); //container
+		});//get JSON
+		// $('#container').mouseenter(function(){
+		// 	$(this).css("overflow","auto");
+		// });
+		// $('#container').mouseleave(function(){
+		// 	$(this).css("overflow","hidden");
+		// });
+	}); //start
+
+///////////////////////////
+// 	var init = function(){
+// 		console.log('Initializing app.');
+// 	};
+
+// 	return {
+// 		init: init
+// 	};
+// })();
+
+
+
+// $.getJSON('results.json', function(data){
+// 	console.log(data);
+// 	var output = '<ul class="searchresults">';
+// 	$.each(data, function(key, val){
+// 		console.log(key);
+// 		console.log(val);
+// 		console.log(val[0].name);
+// 		output += '<li>';
+// 		output += '<h2>' + val.name + '</h2>';
+// 		output += '</li>';
+// 	});
+// 	output += '</ul>';
+// 	$('#update').html(output);
+// });
+
 
